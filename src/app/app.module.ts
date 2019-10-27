ffd66af3737f4925a5559da5ea5ee1742f158f24
Koshin-Mohamed <koshin.moh@gmail.com>
2019-10-27 23:29:10 +0300

    add a component for the form and to display the quotes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule,
