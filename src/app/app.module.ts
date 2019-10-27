980ba54753a939e97c027c072f4f8caf2b012516
Koshin-Mohamed <koshin.moh@gmail.com>
2019-10-27 23:52:33 +0300

    add 3 child components to the display component and basic html layout to app component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { DisplayComponent } from './display/display.component';
+import { AddComponent } from './display/add/add.component';
+import { UpAndDownvoteComponent } from './display/up-and-downvote/up-and-downvote.component';
+import { DeleteComponent } from './display/delete/delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    DisplayComponent
+    DisplayComponent,
+    AddComponent,
+    UpAndDownvoteComponent,
+    DeleteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/display/up-and-downvote/up-and-downvote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-up-and-downvote',
+  templateUrl: './up-and-downvote.component.html',
+  styleUrls: ['./up-and-downvote.component.css']
+})
+export class UpAndDownvoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
