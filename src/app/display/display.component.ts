7eda3690eb8df8d6eb9c10723b26f05ce6753cc7
Koshin-Mohamed <koshin.moh@gmail.com>
2019-10-28 15:28:19 +0300

    add functionality toggle functionality to reveal contributor as well as some styling to the button element and added a details component in the previous commit.

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -9,10 +9,14 @@ import { Quotes } from "../quotes";
 export class DisplayComponent implements OnInit {
 
   quotes: Quotes[] = [
-    new Quotes (1, "Biggie Smalls", "As I leave my competition respirator style, climb the ladder to success escalator style", "Koshin"),
-    new Quotes (2, "Nipsey Hussle", "Never let a hard time humble us", "Salma"),
-    new Quotes (3, "Jay-Z", "Difficulty takes a day impossible takes a week", "Jibril"),
+    new Quotes (1, "Biggie Smalls", "\"As I leave my competition respirator style, climb the ladder to success escalator style\"", "Koshin"),
+    new Quotes (2, "Nipsey Hussle", "\"Never let a hard time humble us\"", "Salma"),
+    new Quotes (3, "Jay-Z", "\"Difficulty takes a day impossible takes a week\"", "Jibril"),
   ];
+
+  toggleUser(index){
+    this.quotes[index].showUser = !this.quotes[index].showUser;
+  }
 
   constructor() { }
 
